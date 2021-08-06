54b9bc5fe63c338f78de178bba941023a6974c62
gnoyixiang <30717323+gnoyixiang@users.noreply.github.com>
2021-08-06 09:11:17 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -35,23 +35,16 @@ document.getElementById("form").addEventListener("submit", (evt) => {
   spiral(num, anticlockwise);
 });
 
-function spiral(
-  num,
-  anticlockwise = false,
-  target = "spiral",
-  slowdown = 50
-) {
-  let current = 0;
-  let dir = anticlockwise ? 1 : 0; // 0 = x, 1 = y
-  let coor = [0, 0];
+function spiral (num, anticlockwise) {
+  let current = 0
+  let moveDir = anticlockwise ? 1 : 0 // 0 = x, 1 = y
+  let coor = [0,0]
 
   // set starting coordinate of axis to -1
-  coor[dir] = -1;
+  coor[moveDir] = -1
 
   // create grid first
-  const grid = Array(num)
-    .fill()
-    .map(() => Array(num).fill(null));
+  const grid = Array(num).fill().map(() => Array(num))
 
   const containerEl = document.getElementById(target);
   if (containerEl) {
@@ -62,36 +55,25 @@ function spiral(
       .join("");
   }
 
-  // get an array of border frames to loop
-  const borders = Array(Math.ceil(num / 2))
-    .fill(null)
-    .map((border, i) => {
-      const dim = num - 2 * i;
-      const dir = dim > 2 ? 4 : dim > 1 ? 3 : 1;
-      return { dim, dir };
-    });
-
   // loop through every border frames
-  for (let b = 0; b < borders.length; b++) {
-    const border = borders[b];
+  for(let dim = num; dim > 0; dim -= 2) {
+    const directions = dim > 2 ? 4 : dim > 1 ? 3 : 1
 
     // loop through every direction
-    for (let d = 0; d < border.dir; d++) {
+    for(let d = 0; d < directions; d++) {
       // offset for 1 direction is always 0, then for next 2 direction is 1 and 4th direction is 2
-      const offset = d > 2 ? 2 : d > 0 ? 1 : 0;
+      const offset = d > 2 ? 2 : d > 0 ? 1 : 0
 
       // inc will be positive for first 2 directions and negative for last 2 directions
-      const inc = d > 1 ? -1 : 1;
-
+      const inc = d > 1 ? -1 : 1
+      
       // loop through the steps for this dimension
-      for (let steps = 0; steps < border.dim - offset; steps++) {
+      for(let steps = 0; steps < dim - offset; steps++) {
         // inc coordinate in dir
-        coor[dir] = coor[dir] + inc;
+        coor[moveDir] = coor[moveDir] + inc 
         // set value in this coordinate
-        const x = coor[0];
-        const y = coor[1];
-        grid[y][x] = ++current;
-
+        grid[coor[1]][coor[0]] = ++current   
+        
         if (containerEl) {
           setTimeout(() => {
             containerEl.childNodes[y].childNodes[x].innerHTML = grid[y][x];
@@ -100,9 +82,9 @@ function spiral(
       }
 
       // change inc direction after finish looping through each direction
-      dir = dir === 0 ? 1 : 0;
+      moveDir = moveDir === 0 ? 1 : 0
     }
   }
-
-  return grid;
+  
+  return grid
 }
