fe3b02acaae65c704c47e7e71ab38db211cf11dd
gnoyixiang <30717323+gnoyixiang@users.noreply.github.com>
2021-08-06 09:19:41 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -35,16 +35,18 @@ document.getElementById("form").addEventListener("submit", (evt) => {
   spiral(num, anticlockwise);
 });
 
-function spiral (num, anticlockwise) {
-  let current = 0
-  let moveDir = anticlockwise ? 1 : 0 // 0 = x, 1 = y
-  let coor = [0,0]
+function spiral(num, anticlockwise = false, target = "spiral", slowdown = 20) {
+  let current = 0;
+  let moveDir = anticlockwise ? 1 : 0; // 0 = x, 1 = y
+  let coor = [0, 0];
 
   // set starting coordinate of axis to -1
-  coor[moveDir] = -1
+  coor[moveDir] = -1;
 
   // create grid first
-  const grid = Array(num).fill().map(() => Array(num))
+  const grid = Array(num)
+    .fill()
+    .map(() => Array(num).fill(null));
 
   const containerEl = document.getElementById(target);
   if (containerEl) {
@@ -56,24 +58,26 @@ function spiral (num, anticlockwise) {
   }
 
   // loop through every border frames
-  for(let dim = num; dim > 0; dim -= 2) {
-    const directions = dim > 2 ? 4 : dim > 1 ? 3 : 1
+  for (let dim = num; dim > 0; dim -= 2) {
+    const directions = dim > 2 ? 4 : dim > 1 ? 3 : 1;
 
     // loop through every direction
-    for(let d = 0; d < directions; d++) {
+    for (let d = 0; d < directions; d++) {
       // offset for 1 direction is always 0, then for next 2 direction is 1 and 4th direction is 2
-      const offset = d > 2 ? 2 : d > 0 ? 1 : 0
+      const offset = d > 2 ? 2 : d > 0 ? 1 : 0;
 
       // inc will be positive for first 2 directions and negative for last 2 directions
-      const inc = d > 1 ? -1 : 1
-      
+      const inc = d > 1 ? -1 : 1;
+
       // loop through the steps for this dimension
-      for(let steps = 0; steps < dim - offset; steps++) {
+      for (let steps = 0; steps < dim - offset; steps++) {
         // inc coordinate in dir
-        coor[moveDir] = coor[moveDir] + inc 
+        coor[moveDir] = coor[moveDir] + inc;
+        const x = coor[0];
+        const y = coor[1];
         // set value in this coordinate
-        grid[coor[1]][coor[0]] = ++current   
-        
+        grid[y][x] = ++current;
+
         if (containerEl) {
           setTimeout(() => {
             containerEl.childNodes[y].childNodes[x].innerHTML = grid[y][x];
@@ -82,9 +86,9 @@ function spiral (num, anticlockwise) {
       }
 
       // change inc direction after finish looping through each direction
-      moveDir = moveDir === 0 ? 1 : 0
+      moveDir = moveDir === 0 ? 1 : 0;
     }
   }
-  
-  return grid
+
+  return grid;
 }
